84abae393f4abbba62e65d6fd20822c2f3228843
rajkdh <112664790+rajkdh@users.noreply.github.com>
2023-01-18 20:40:52 -0500

    added basic html code.

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// add start button - timer starts and questions are displayed.
+//back to back questions
+//reduce time everytime there is a wrong answer. 
+// game ends when the timer runs out or all questions are answered.
+//save initial and score and display on score board.
+//button to restart game.
+//keep score displayed. 
