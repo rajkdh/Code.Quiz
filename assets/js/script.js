656015c94e182b4dfdca15a0aca4b073dab39e4b
Rajvir Kaur Dhanoa <rajd@Rajvirs-MBP.ht.home>
2023-01-21 18:56:13 -0500

    added code

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,3 +5,18 @@
 //save initial and score and display on score board.
 //button to restart game.
 //keep score displayed. 
+var listEl;
+var buttonEl;
+var timeStart = 75;
+var questionsAnswer;
+var questionsScore = 0;
+var leaderBoard = [];
+var x = 0;
+
+var startButton = document.querySelector("#start-quiz");
+var quizContainer = document.querySelector("#quiz-box");
+var quizButton = document.querySelector(".btn");
+var timer = document.querySelector("#timer");
+var quizHeader = document.querySelector("#quiz-header");
+var quizText = document.querySelector("#quiz-text");
+var highScore = document.querySelector("#high-score");
