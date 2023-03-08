c1bd6506348af720fcf59e24c379ce0fbfd29f5b
rajkdh <112664790+rajkdh@users.noreply.github.com>
2023-03-07 20:28:29 -0500

    added js code

--- assets/js/script.js
+++ assets/js/script.js
@@ -23,149 +23,18 @@ var questionsEl = document.querySelector("#question-title");
 var choicesEl = document.querySelector("#choices");
 var highScoreEl = document.querySelector("#high-score");
 
-// List of questions, choices, and answers
-var questions = [
-    {
-      title: "Which one is a looping structure in JavaScript?",
-      choices: ["All the below", "For", "While", "do-while loops"],
-      answer: "All the below"
-    },
-    {
-      title: "What are the two basic groups of data types in JavaScript?",
-      choices: [
-        "Primitive and attribute",
-        "Primitive and reference types",
-        "Reference types and attribute",
-        "None of the above"
-      ],
-      answer: "Primitive and reference types"
-    },
-    {
-      title: "Commonly used data types DO NOT include:",
-      choices: ["strings", "booleans", "alerts", "numbers"],
-      answer: "alerts"
-    },
-    {
-      title: "Boolean operators that can be used in JavaScript include:",
-      choices: [
-        "'And' Operator &&",
-        "'Or' Operator ||",
-        "'Not' Operator !",
-        "All the above"
-      ],
-      answer: "All the above"
-    },
-    {
-      title:
-        "Which one of these is not among the three different types of errors in JavaScript?",
-      choices: [
-        "Animation time errors",
-        "Load time errors",
-        "Run time errors",
-        "Logical Errors"
-      ],
-      answer: "Animation time errors"
-    },
-    {
-      title: "What is the data type of variables in JavaScript?",
-      choices: [
-        "Object data types",
-        "Function data type",
-        "None of the above",
-        "All of the above"
-      ],
-      answer: "Object data types"
-    },
-    {
-      title: "The condition in an if / else statement is enclosed within ____.",
-      choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
-      answer: "parentheses"
-    },
-    {
-      title: "Arrays in JavaScript can be used to store ____.",
-      choices: [
-        "numbers and strings",
-        "other arrays",
-        "booleans",
-        "all of the above"
-      ],
-      answer: "all of the above"
-    },
-    {
-      title:
-        "String values must be enclosed within ____ when being assigned to variables.",
-      choices: ["commas", "curly brackets", "quotes", "parentheses"],
-      answer: "quotes"
-    },
-    {
-      title:
-        "A very useful tool used during development and debugging for printing content to the debugger is:",
-      choices: ["JavaScript", "terminal / bash", "for loops", "console.log"],
-      answer: "console.log"
-    },
-    {
-      title: "What is the type of Pop up boxes available in JavaScript?:",
-      choices: ["Alert", "Confirm", "Prompt", "All the above"],
-      answer: "All the above"
-    }
-  ];
-
-// Quiz variable:
-var currentQuestionIndex = 0;
-var time = questions.length * 15;
-var timerId;
-
-// add event listener for start button to open game page
-quizButton.addEventListener("click", startGame)
-
+//add event listener to start button to send user to game page
+startButtonEl.addEventListener("click", startGame)
 function startGame() {
-    window.location.href = "index.html";
-    timerId = setInterval(clockTick, 1000);
-    timer.textContent = timer;
-    questions();
-}
-// start timer
-
-function startQuiz() {
-  // hide start screen
-  var startScreenEl = document.getElementById("start-screen");
-  startScreenEl.setAttribute("class", "hide");
-
-  // un-hide questions section
-  questionsEl.removeAttribute("class");
-
-  // start timer
-  timerId = setInterval(clockTick, 1000);
-
-  // show starting time
-  timerEl.textContent = time;
-
-  getQuestion();
-}
-function getQuestion() {
-  // get current question object from array
-  var currentQuestion = questions[currentQuestionIndex];
-
-  // update title with current question
-  var titleEl = document.getElementById("question-title");
-  titleEl.textContent = currentQuestion.title;
-
-  // clear out any old question choices
-  choicesEl.innerHTML = "";
-
-  // loop over choices
-  currentQuestion.choices.forEach(function(choice, i) {
-    // create new button for each choice
-    var choiceNode = document.createElement("button");
-    choiceNode.setAttribute("class", "choice");
-    choiceNode.setAttribute("value", choice);
-
-    choiceNode.textContent = i + 1 + ". " + choice;
-
-    // attach click event listener to each choice
-    choiceNode.onclick = questionClick;
-
-    // display on the page
-    choicesEl.appendChild(choiceNode);
-  });
+  window.location.href = "index.html";
+  var startTime = setInterval(function(){
+    if (timeStart <= -1 || x === 5) {
+      clearInterval(startTime);
+      endGame();
+    } else {
+    timerEl.textContent = timeStart;
+  }
+  //timeStart---;
+//}, 666);
+firstQuestion();
 }
