00b13376750099fd3ed46c9b58364f352bb2294d
rajkdh <112664790+rajkdh@users.noreply.github.com>
2023-03-11 22:51:19 -0500

    removed extra files

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,26 +15,3 @@ var questionsScore = 0;
 var leaderBoard = [];
 var x = 0;
 
-var startButtonEl = document.querySelector("#start-quiz");
-var gameContainerEl = document.querySelector(".game-container");
-var quizButtonEl = document.querySelector(".btn");
-var timerEl = document.querySelector("#timer");
-var questionsEl = document.querySelector("#question-title");
-var choicesEl = document.querySelector("#choices");
-var highScoreEl = document.querySelector("#high-score");
-
-//add event listener to start button to send user to game page
-startButtonEl.addEventListener("click", startGame)
-function startGame() {
-  window.location.href = "index.html";
-  var startTime = setInterval(function(){
-    if (timeStart <= -1 || x === 5) {
-      clearInterval(startTime);
-      endGame();
-    } else {
-    timerEl.textContent = timeStart;
-  }
-  //timeStart---;
-//}, 666);
-firstQuestion();
-}
